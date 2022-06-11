84d329881c09ec7b4795b32b188d015ab8f57aad
Jennifer Lu <julenn@gmail.com>
2022-06-10 18:16:34 -0700

    added captions

--- scripts/script.js
+++ scripts/script.js
@@ -2,26 +2,36 @@ $(document).ready(function(){
 
   console.log("hello begin jquery =)");
 
-  var isWorkCurrentPage = true;
-  var isPlayCurrentPage = false;
-	var isAboutCurrentPage = false;
-
   $("#workButton").on('click', function(){
-    console.log("clicked work button")
+    $("#workButton").addClass('selected');
+    $("#aboutButton").removeClass('selected');
     $("#work").css('display','grid').css('transition','0.2s');
     $("#play").css('display','none');
     $("#about").css('display','none');
   });
 
   $("#playButton").on('click', function(){
-    console.log("clicked play button")
+    $("#playButton").addClass('selected');
     $("#work").css('display','none');
     $("#play").css('display','block').css('transition','0.2s');
     $("#about").css('display','none');
   });
 
   $("#aboutButton").on('click', function(){
-    console.log("clicked about button")
+    $("#aboutButton").addClass('selected');
+    $("#workButton").removeClass('selected');
+    // var app = document.getElementById('intro');
+    // var typewriter = new
+    // Typewriter(app, {
+    //   loop: false,
+    //   delay: 75,
+    // });
+    // typewriter
+    // .pauseFor(1200)
+    // .typeString('<strong>Hello world! </strong>')
+    // .pauseFor(60)
+    // .typeString('Welcome to my site.')
+    // .start();
     $("#work").css('display','none');
     $("#play").css('display','none');
     $("#about").css('display','block').css('transition','0.2s');
@@ -29,4 +39,34 @@ $(document).ready(function(){
 
 	// Make Work page current page
 	$("#workPage").attr("class", "currentPage");
+
+  $(".btn-recall").click(function(){
+      $(".recall").collapse('toggle');
+      $(".btn-recall").toggleClass('active');
+    });
+  $(".btn-whales").click(function(){
+      $(".whales").collapse('toggle');
+      $(".btn-whales").toggleClass('active');
+    });
+    $(".btn-lintel").click(function(){
+      $(".lintel").collapse('toggle');
+      $(".btn-lintel").toggleClass('active');
+    });
+    $(".btn-vaxxy").click(function(){
+      $(".vaxxy").collapse('toggle');
+      $(".btn-vaxxy").toggleClass('active');
+    });
+    $(".btn-tule").click(function(){
+      $(".tule").collapse('toggle');
+      $(".btn-tule").toggleClass('active');
+    });
+    $(".btn-porpoise").click(function(){
+      $(".porpoise").collapse('toggle');
+      $(".btn-porpoise").toggleClass('active');
+    });
+    $(".btn-virus").click(function(){
+      $(".virus").collapse('toggle');
+      $(".btn-virus").toggleClass('active');
+    });
+
 });
